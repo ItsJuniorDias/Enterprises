60d2776e2b3105c74c62131a533484f057dee3a3
AlexandreJr <juniordias_@live.com>
2023-03-02 20:55:17 -0300

    chore: rename screens

--- enterprise/src/routes/index.tsx
+++ enterprise/src/routes/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import { Home, Show, SignIn } from '../pages';
+import { Home, Show, SignIn } from '../screens';
 
 const Auth = createStackNavigator();
 
