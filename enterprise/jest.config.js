58c2324939c1ff0c43e40e1a94fac6e25d1efb50
AlexandreJr <juniordias_@live.com>
2023-03-02 15:32:51 -0300

    chore: adjust jest json

--- enterprise/jest.config.js
+++ enterprise/jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
   preset: 'react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  coveragePathIgnorePatterns: [
+    'index.js',
+    'index.jsx',
+    'index.ts',
+    'types.ts',
+    'rootReducer.ts',
+    'assets',
+  ],
 };
